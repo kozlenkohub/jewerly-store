0ad3d1cf601dbfe39d0c942f5ff219beecd246bd
Nikita <ggkozlenko@gmail.com>
2025-01-23 20:46:21 +0100

    cartel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { ShopProvider } from './context/shopContext.jsx';
+import { ShopProvider } from './context/shopContext';
 
 createRoot(document.getElementById('root')).render(
   <ShopProvider>
