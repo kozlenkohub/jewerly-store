e573bf3c1d7c98e28bd52703fbefbde868d6a4ad
Nikita <ggkozlenko@gmail.com>
2025-01-23 22:25:59 +0100

    xxx

--- frontend/src/components/LatestCatalog.jsx
+++ frontend/src/components/LatestCatalog.jsx
@@ -22,17 +22,17 @@ const LatestCatalog = () => {
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nulla tempora expedita
           laboriosam
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 sm:gap-4 gap-y-4 sm:gap-y-6 mt-6 ">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 sm:gap-4 gap-y-4 sm:gap-y-6 mt-6 ">
           {latestProducts.map((product) => (
             <ProductItem key={product._id} {...product} />
           ))}
-          <div className="col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-5 text-center mt-4">
-            <a
-              href="/cart"
-              className="text-[20px] hover:underline transition duration-300 ease-in-out transform hover:scale-105">
-              See more
-            </a>
-          </div>
+        </div>
+        <div className="col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-5 text-center mt-4">
+          <a
+            href="/cart"
+            className="text-[20px] hover:underline transition duration-300 ease-in-out transform hover:scale-105">
+            See more
+          </a>
         </div>
       </div>
     </div>
