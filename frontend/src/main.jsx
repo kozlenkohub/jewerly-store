d3619d53aed73bfe3f935ac7e59de1313165167b
Nikita <ggkozlenko@gmail.com>
2025-01-23 20:41:07 +0100

    xxx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { ShopProvider } from './context/shopContext';
+import { ShopProvider } from './context/shopContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <ShopProvider>
