fa4ddd8ce3c6212a384e3bc32f0534f0f185d08e
Nikita <ggkozlenko@gmail.com>
2025-01-24 01:42:45 +0100

    xxx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,6 +26,7 @@ const Navbar = () => {
   const backgroundOpacity = Math.min(scrollY / 300, 1);
   const textColor = scrollY > 150 ? 'text-gray-700' : 'text-white';
   const boxShadow = scrollY > 150 ? 'shadow-md' : '';
+  const iconColor = scrollY > 150 ? 'text-mainColor' : 'text-white';
 
   return (
     <div
@@ -56,9 +57,9 @@ const Navbar = () => {
             </NavLink>
           </ul>
           <div className="flex items-center gap-6">
-            <FaSearch className="w-5 h-5 cursor-pointer sm:block hidden" />
-            <div className="group relative sm:block hidden">
-              <FaUser className="w-5 h-5 cursor-pointer" />
+            <FaSearch className={`w-5 h-5 cursor-pointer sm:block hidden ${iconColor}`} />
+            <div className={`group relative sm:block hidden ${iconColor}`}>
+              <FaUser className={`w-5 h-5 cursor-pointer ${iconColor}`} />
               <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-2">
                 <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                   <p className="cursor-pointer hover:text-black">My Profile</p>
@@ -67,8 +68,8 @@ const Navbar = () => {
                 </div>
               </div>
             </div>
-            <Link to="/cart" className="relative sm:block hidden">
-              <FaShoppingCart className="w-5 h-5" />
+            <Link to="/cart" className={`relative sm:block hidden ${iconColor}`}>
+              <FaShoppingCart className={`w-5 h-5 ${iconColor}`} />
               <p className="absolute right-[-12px] bottom-[-9px] w-[20px] h-[20px] text-center leading-4 bg-secondaryColor text-white aspect-square rounded-full text-[12px]">
                 10
               </p>
@@ -76,9 +77,7 @@ const Navbar = () => {
 
             <FaBars
               onClick={() => setVisible(true)}
-              className={`w-5 h-5 cursor-pointer sm:hidden transition-colors duration-300 ${
-                scrollY > 150 ? 'text-mainColor' : 'text-white'
-              }`}
+              className={`w-5 h-5 cursor-pointer sm:hidden transition-colors duration-300 ${iconColor}`}
             />
           </div>
           <div
@@ -88,13 +87,13 @@ const Navbar = () => {
             <div className="flex flex-col">
               <div className="flex items-center justify-between p-3">
                 <div onClick={() => setVisible(false)} className="flex items-center gap-1">
-                  <MdOutlineKeyboardArrowLeft className="w-6 h-6" />
-                  <p>Back</p>
+                  <MdOutlineKeyboardArrowLeft className="w-6 h-6 text-mainColor" />
+                  <p className="text-mainColor">Back</p>
                 </div>
                 <div className="flex items-center gap-6">
-                  <FaSearch className="w-5 h-5 cursor-pointer" />
-                  <div className="group relative">
-                    <FaUser className="w-5 h-5 cursor-pointer" />
+                  <FaSearch className="w-5 h-5 cursor-pointer text-mainColor" />
+                  <div className="group relative text-mainColor">
+                    <FaUser className="w-5 h-5 cursor-pointer text-mainColor" />
                     <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-2">
                       <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                         <p className="cursor-pointer hover:text-black">My Profile</p>
@@ -103,11 +102,8 @@ const Navbar = () => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/cart" className="relative">
-                    <FaShoppingCart className="w-5 h-5" />
-                    {/* <p className="absolute right-[-12px] bottom-[-9px] w-[20px] h-[20px] text-center leading-4 bg-[#923f3f] text-white aspect-square rounded-full text-[12px]">
-                      10
-                    </p> */}
+                  <Link to="/cart" className="relative text-mainColor">
+                    <FaShoppingCart className="w-5 h-5 text-mainColor" />
                   </Link>
                 </div>
               </div>
