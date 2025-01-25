fa5e5ed6f3a4a73ec2ece76e8c2d7aff48ca957c
Nikita <ggkozlenko@gmail.com>
2025-01-25 23:56:35 +0100

    ччч

--- frontend/src/components/NewsletterBox/index.jsx
+++ frontend/src/components/NewsletterBox/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import video from '../../assets/night.webm'; // Импортируем видео
 import './NewsletterBox.css'; // Импортируем файл стилей
 
 const NewsletterBox = () => {
@@ -7,21 +8,27 @@ const NewsletterBox = () => {
   };
   return (
     <div className="newsletter-box text-center">
-      <p className="text-2xl font-medium text-gray-800">Subscribe now & get 20% off</p>
-      <p className="text-gray-400 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      <form
-        onSubmit={onSubmit}
-        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6  pl-3">
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full sm:flex-1 outline-none bg-white/40 text-black  border-mainColor py-2"
-          required
-        />
-        <button type="submit" className="bg-mainColor text-white text-xs px-10 py-4">
-          Subscribe
-        </button>
-      </form>
+      <video autoPlay muted loop>
+        <source src={video} type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="overlay">
+        <p className="text-2xl font-medium text-white">Subscribe now & get 20% off</p>
+        <p className="text-white mt-3">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <form
+          onSubmit={onSubmit}
+          className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6  pl-3">
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full sm:flex-1 outline-none bg-white/70 text-black border-mainColor py-2" // Adjusted background opacity
+            required
+          />
+          <button type="submit" className="bg-mainColor text-white text-xs px-10 py-4">
+            Subscribe
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
