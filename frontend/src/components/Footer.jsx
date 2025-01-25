9631296ab59e67eedf43f4c23d44c2ca24d16c97
Nikita <ggkozlenko@gmail.com>
2025-01-25 23:19:00 +0100

    xxx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
 
   return (
     <div className="bg-mainColor">
-      <div className="flex pt-10 flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-4 my-10 text-sm max-w-[1280px] mx-auto px-4">
+      <div className="flex pt-10 flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-4 mt-10 mb-7 text-sm max-w-[1280px] mx-auto px-4">
         <div className="">
           <p className="text-2xl text-white mb-4">LOGO</p>
           <p className="w-full md:w-2/3 text-slate-300 font-light poppins text-[12px]">
