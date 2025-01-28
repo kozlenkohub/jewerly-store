6ba4cce316b1984c8ff4eb4828f1e096afa59f89
Nikita <ggkozlenko@gmail.com>
2025-01-28 20:52:21 +0100

    xxx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -35,7 +35,7 @@ const SearchBar = () => {
   }
 
   return isOpenSearch ? (
-    <div className="border-t border-b bg-gray-50 mt-20 text-center">
+    <div className="mt-20 text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
           className="flex-1 outline-none focus:outline-none bg-inherit text-sm border-none"
@@ -46,7 +46,7 @@ const SearchBar = () => {
         />
         <FaSearch className="w-4" />
       </div>
-      <FaTimes className="inline w-3 cursor-pointer" onClick={handleClear} />
+      {search && <FaTimes className="inline w-3 cursor-pointer" onClick={handleClear} />}
     </div>
   ) : null;
 };
