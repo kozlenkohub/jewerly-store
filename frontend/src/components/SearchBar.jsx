41ac89b3eb7163c6d72075650f51647d10d36b9c
Nikita <ggkozlenko@gmail.com>
2025-01-28 21:10:57 +0100

    ччч

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -27,7 +27,7 @@ const SearchBar = () => {
     <div className="mt-12 text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
-          className="flex-1 outline-none focus:outline-none bg-inherit text-sm border-none"
+          className="flex-1 bg-inherit text-sm border-none outline-none focus:ring-0"
           type="text"
           placeholder="Search"
           onChange={handleChange}
