9dc79355f1105478d0450bbaee003b581f4f2466
Nikita <ggkozlenko@gmail.com>
2025-01-28 20:56:47 +0100

    xxx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -35,7 +35,7 @@ const SearchBar = () => {
   }
 
   return isOpenSearch ? (
-    <div className="mt-20 text-center">
+    <div className="mt-12 text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
           className="flex-1 outline-none focus:outline-none bg-inherit text-sm border-none"
