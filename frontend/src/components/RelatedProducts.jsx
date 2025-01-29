c05d0ef2e0134343d77dcddca6186236f1671b9b
Nikita <ggkozlenko@gmail.com>
2025-01-29 14:02:16 +0100

    xxx

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -41,7 +41,7 @@ const RelatedProducts = () => {
   return (
     <div className="my-24">
       <div className="text-center text-3xl py-2">
-        <Title text1={'Related'} text2={'Prodcuts'} />
+        <Title text1={'Related'} text2={'Products'} />
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 text-center">
         {related && related.map((product, index) => <ProductItem key={index} {...product} />)}
