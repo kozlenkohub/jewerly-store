32c07ff77f3a08c5f17dcb3d7f09b7d872ab611f
Nikita <ggkozlenko@gmail.com>
2025-01-30 02:00:16 +0100

    xxxx

--- frontend/src/pages/Catalog.jsx
+++ frontend/src/pages/Catalog.jsx
@@ -128,7 +128,7 @@ const Catalog = () => {
 
           {isLoading ? (
             <div className="flex justify-center items-center sm:absolute top-1/2 right-[44%]  ">
-              <DotLoader size={50} color={'#490d47'} loading={isLoading} speedMultiplier={0.5} />
+              <DotLoader size={50} color={'#8c2d60'} loading={isLoading} speedMultiplier={0.5} />
             </div>
           ) : (
             <div
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        mainColor: '#490d47',
-        secondaryColor: '#490d47',
+        mainColor: '#8c2d60',
+        secondaryColor: '#8c2d60',
         textColor: '#383838',
       },
     },
