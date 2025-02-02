1b0ab667efc9ad98e43c0f93af61753578d12fb6
Nikita <ggkozlenko@gmail.com>
2025-02-02 23:29:45 +0100

    xx

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    port: 5174,
+  plugins: [react()],
+  server: { port: 5174 },
+
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: '/', // Важно для корректной работы маршрутов
+  build: {
+    outDir: 'dist', // Папка для сборки
   },
 });
