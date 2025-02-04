2b2381ca541d9b9c75acacee51de766ed66cb21a
Nikita <ggkozlenko@gmail.com>
2025-02-04 02:55:52 +0100

    хххх

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -105,7 +105,7 @@ orderSchema.pre('save', function (next) {
 
 // Статический метод для удаления просроченных заказов
 orderSchema.statics.removeExpiredProcessingOrders = async function () {
-  const fiveMinutesAgo = new Date(Date.now() - 1 * 60 * 1000);
+  const fiveMinutesAgo = new Date(Date.now() - 3 * 60 * 1000);
 
   try {
     const result = await this.deleteMany({
