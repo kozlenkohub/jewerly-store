b941c271f4fbef81f06ac2e7a55d20816e53d413
Nikita <ggkozlenko@gmail.com>
2025-02-04 18:06:39 +0100

    xx

---
+++ backend/utils/enums.js
@@ -0,0 +1,61 @@
+// cutFormEnum.js
+
+export const cutFormEnum = [
+  {
+    value: 'asher',
+    label: { en: 'Asher', ru: 'Ашер' },
+  },
+  {
+    value: 'pear',
+    label: { en: 'Pear', ru: 'Груша' },
+  },
+  {
+    value: 'round',
+    label: { en: 'Round', ru: 'Круг' },
+  },
+  {
+    value: 'cushion',
+    label: { en: 'Cushion', ru: 'Кушон' },
+  },
+  {
+    value: 'marquise',
+    label: { en: 'Marquise', ru: 'Маркиз' },
+  },
+  {
+    value: 'oval',
+    label: { en: 'Oval', ru: 'Овал' },
+  },
+  {
+    value: 'princess',
+    label: { en: 'Princess', ru: 'Принцесса' },
+  },
+  {
+    value: 'radiant',
+    label: { en: 'Radiant', ru: 'Радиант' },
+  },
+  {
+    value: 'heart',
+    label: { en: 'Heart', ru: 'Сердце' },
+  },
+  {
+    value: 'emerald',
+    label: { en: 'Emerald', ru: 'Изумруд' },
+  },
+];
+
+// metalEnum.js
+
+export const metalEnum = [
+  {
+    value: 'white gold',
+    label: { en: 'white gold', ru: 'Белое золото' },
+  },
+  {
+    value: 'yellow gold',
+    label: { en: 'yellow gold', ru: 'Жёлтое золото' },
+  },
+  {
+    value: 'rose gold',
+    label: { en: 'rose gold', ru: 'Розовое золото' },
+  },
+];
