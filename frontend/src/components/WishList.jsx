20afed64551e74a1f7b87a5b1b86bb5f19218cf1
Nikita <ggkozlenko@gmail.com>
2025-02-12 21:35:18 +0100

    xxxx

--- frontend/src/components/WishList.jsx
+++ frontend/src/components/WishList.jsx
@@ -17,11 +17,14 @@ const WishList = () => {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 futura">
       <h2 className="text-2xl font-medium mb-6">{t('wishlist.title')}</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {wishlistItems.map((product) => (
-          <ProductItem key={product._id} {...product} />
+      <div
+        className={`grid py grid-cols-2 text-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4 gap-y-4 sm:gap-y-6 mt-6 min-h-[44vh] w-[1000px] ${
+          wishlistItems.length === 0 ? 'px-4' : ''
+        }`}>
+        {wishlistItems.map((item, index) => (
+          <ProductItem key={index} {...item} />
         ))}
       </div>
     </div>
