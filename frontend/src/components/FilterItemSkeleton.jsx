8756303c5f5646a09523be4611a06758aff84b68
Nikita <ggkozlenko@gmail.com>
2025-02-13 21:51:26 +0100

    xxx

--- frontend/src/components/FilterItemSkeleton.jsx
+++ frontend/src/components/FilterItemSkeleton.jsx
@@ -4,7 +4,7 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 const FilterItemSkeleton = () => {
   return (
-    <div className="filter-item flex gap-8 flex-col">
+    <div className="filter-item flex gap-8 flex-col mt-6">
       <Skeleton height={20} count={2} />
       <Skeleton height={20} count={2} />
     </div>
